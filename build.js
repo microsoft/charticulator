7e08093d9b23cc12a999859c5414b8e414dc17ae
Donghao Ren <donghao.ren@gmail.com>
2018-09-07 01:01:05 -0700

    Update multirun version and polish build.js

--- build.js
+++ build.js
@@ -2,107 +2,131 @@ const fs = require("fs-extra");
 const jsyaml = require("js-yaml");
 const multirun = require("multirun");
 
+/** Convert a YAML file to JSON */
 function yamlToJSON(yamlFile, jsonFile) {
-    return fs.readFile(yamlFile).then((contents) => {
-        let doc = jsyaml.safeLoad(contents);
-        let json = JSON.stringify(doc);
-        return fs.writeFile(jsonFile, new Buffer(json, "utf-8"));
-    });
+  return fs.readFile(yamlFile).then((contents) => {
+    let doc = jsyaml.safeLoad(contents);
+    let json = JSON.stringify(doc);
+    return fs.writeFile(jsonFile, Buffer.from(json, "utf-8"));
+  });
 }
 
-function yamlToJavaScript(yamlFile, javascriptFile, variableName, addlConfig = {}) {
-    return fs.readFile(yamlFile).then((contents) => {
-        let doc = {
-            ...(jsyaml.safeLoad(contents)),
-            ...addlConfig
-        };
-        let json = JSON.stringify(doc);
-        let javascript = `var ${variableName} = ${json};\n`;
-        return fs.writeFile(javascriptFile, new Buffer(javascript, "utf-8"));
-    });
-}
-
-function copyFolder(folder1, folder2) {
-    if (fs.existsSync(folder1)) {
-        fs.copy(folder1, folder2);
+/** Convert a YAML file to JavaScript variable */
+function yamlToJavaScript(yamlFile, javascriptFile, variableName, mixin) {
+  return fs.readFile(yamlFile).then((contents) => {
+    let doc = jsyaml.safeLoad(contents);
+    if (mixin != undefined) {
+      mixin(doc);
     }
+    let json = JSON.stringify(doc);
+    let javascript = `var ${variableName} = ${json};\n`;
+    return fs.writeFile(javascriptFile, Buffer.from(javascript, "utf-8"));
+  });
 }
 
-const isProd = process.env.NODE_ENV === 'production';
+/** Copy folder1 to folder2 */
+function copyFolder(folder1, folder2) {
+  if (fs.existsSync(folder1)) {
+    fs.copy(folder1, folder2);
+  }
+}
+
+// Parse environment variable
+const isProd = process.env.NODE_ENV === "production";
+
+// The default dev sequence
+const devSequence = ["cleanup", "makedirs", "copy", "third_party_data", "config", "pegjs", "typescript", "sass", "webpack"];
 
 let COMMANDS = {
 
-    // Remove the entire build directory
-    cleanup: () => fs.remove("dist"),
+  // Remove the entire build directory
+  cleanup: () => fs.remove("dist"),
 
-    // Create necessary directories
-    makedirs: [
-        () => fs.mkdirs("dist/styles"),
-        () => fs.mkdirs("dist/data"),
-        () => fs.mkdirs("dist/scripts/core/expression")
-    ],
+  // Create necessary directories
+  makedirs: [
+    () => fs.mkdirs("dist/styles"),
+    () => fs.mkdirs("dist/data"),
+    () => fs.mkdirs("dist/scripts/core/expression")
+  ],
 
-    // Copy files
-    copy: [
-        () => fs.copy("src/core/expression/parser.d.ts", "dist/scripts/core/expression/parser.d.ts"),
+  // Copy files
+  copy: [
+    () => fs.copy("src/core/expression/parser.d.ts", "dist/scripts/core/expression/parser.d.ts"),
 
-        // Copy all of the public files
-        () => copyFolder("./public", "./dist"),
+    // Copy all of the public files
+    () => copyFolder("./public", "./dist"),
 
-        // Copy all of the extensions
-        () => copyFolder("./extensions", "./dist/extensions"),
+    // Copy all of the extensions
+    () => copyFolder("./extensions", "./dist/extensions"),
 
-        // Copy all of the datasets
-        () => copyFolder("./datasets", "./dist/datasets"),
-    ],
+    // Copy all of the datasets
+    () => copyFolder("./datasets", "./dist/datasets"),
+  ],
 
-    // Convert the THIRD_PARTY.yml to json
-    third_party_data: () => yamlToJSON("THIRD_PARTY.yml", "dist/data/THIRD_PARTY.json"),
+  // Convert the THIRD_PARTY.yml to json
+  third_party_data: () => yamlToJSON("THIRD_PARTY.yml", "dist/data/THIRD_PARTY.json"),
 
-    // Convert the config.yml to config.js
-    config: () => {
-        let mixin = {};
-        if (fs.existsSync('datasets/files.json')) {
-            mixin.SampleDatasets = JSON.parse(fs.readFileSync('datasets/files.json'));
-            mixin.SampleDatasets.forEach(dataset => {
-                dataset.tables.forEach(table => {
-                    table.url = 'datasets/' + table.url;
-                });
-            });
+  // Convert the config.yml to config.js
+  config: async () => {
+    let mixin = (doc) => {
+      if (fs.existsSync("datasets/files.json")) {
+        let sampleDatasets = JSON.parse(fs.readFileSync("datasets/files.json"));
+        sampleDatasets.forEach(dataset => {
+          dataset.tables.forEach(table => {
+            table.url = "datasets/" + table.url;
+          });
+        });
+        if (doc.SampleDatasets) {
+          doc.SampleDatasets = doc.SampleDatasets.concat(sampleDatasets);
+        } else {
+          doc.SampleDatasets = sampleDatasets;
         }
-        yamlToJavaScript("config.yml", "dist/data/config.js", "CHARTICULATOR_CONFIG", mixin)
-    },
+      }
+    }
+    await yamlToJavaScript("config.yml", "dist/data/config.js", "CHARTICULATOR_CONFIG", mixin);
+  },
 
-    // Compile sass files
-    sass: [
-        "node-sass sass/app.scss -o dist/styles",
-        "node-sass sass/page.scss -o dist/styles"
-    ],
+  // Compile sass files
+  sass: {
+    app: "node-sass sass/app.scss -o dist/styles",
+    page: "node-sass sass/page.scss -o dist/styles"
+  },
 
-    // Compile the PEGJS parser
-    pegjs: [
-        "pegjs --format commonjs --allowed-start-rules start,start_text -o dist/scripts/core/expression/parser.js src/core/expression/parser.pegjs",
-    ],
+  // Compile the PEGJS parser
+  pegjs: "pegjs --format commonjs --allowed-start-rules start,start_text -o dist/scripts/core/expression/parser.js src/core/expression/parser.pegjs",
 
-    // Compile TypeScript
-    typescript: "tsc",
+  // Compile TypeScript
+  typescript: "tsc",
 
-    // Produce webpack bundles
-    webpack: "webpack --mode=" + (isProd ? "production" : "development"),
+  // Produce webpack bundles
+  webpack: "webpack --mode=" + (isProd ? "production" : "development"),
 
-    server: "http-server ./dist -a 127.0.0.1 -p 4000 -c-1 -s",
-    public_server: "http-server ./dist -a 0.0.0.0 -p 4000 -c-1 -s",
-    watch: [
-        "tsc -w",
-        "webpack -w --mode=" + (isProd ? "production" : "development"),
-        "node-sass --watch sass/app.scss sass/page.scss -o dist/styles",
-        () => multirun.run(COMMANDS["server"])
-    ]
+  server: "http-server ./dist -a 127.0.0.1 -p 4000 -c-1 -s",
+  public_server: "http-server ./dist -a 0.0.0.0 -p 4000 -c-1 -s",
+  watch: {
+    tsc: "tsc -w",
+    webpack: "webpack -w --mode=" + (isProd ? "production" : "development"),
+    sass: "node-sass --watch sass/app.scss sass/page.scss -o dist/styles",
+    server: "http-server ./dist -a 127.0.0.1 -p 4000 -c-1 -s"
+  },
+
+  dev: () => runCommands(devSequence)
 };
+
+/** Run the specified commands names in sequence */
+async function runCommands(sequence) {
+  for (const cmd of sequence) {
+    console.log("Build: " + cmd);
+    await multirun.run(COMMANDS[cmd]);
+  }
+}
 
 // Execute the specified commands, with no args, run the default sequence
 let sequence = process.argv.slice(2);
 if (sequence.length == 0) {
-    sequence = ["cleanup", "makedirs", "copy", "third_party_data", "config", "pegjs", "typescript", "sass", "webpack"];
+  sequence = devSequence;
 }
-multirun.runCommands(COMMANDS, sequence, "Build");
+runCommands(sequence).catch((e) => {
+  console.log(e.message);
+  process.exit(-1);
+});
