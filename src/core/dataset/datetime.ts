262a0f3d02302d293518688dd5011d03565c213f
Donghao Ren <donghao.ren@gmail.com>
2018-09-19 09:10:54 -0700

    Support ISO8601 dates, expose dataset helper functions (#61)
    * In parseDate support a subset of ISO8601 dates (resolves #60)
    
    * Add a function to help convert column values to desired types

--- src/core/dataset/datetime.ts
+++ src/core/dataset/datetime.ts
@@ -21,6 +21,17 @@
 export function parseDate(str: string) {
   str = str.trim();
   let m;
+  // ISO8601 full date: https://stackoverflow.com/a/37563868
+  if (
+    str.match(/^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?(([+-]\d\d:\d\d)|Z)?$/i)
+  ) {
+    const t = Date.parse(str);
+    if (!isNaN(t)) {
+      return t;
+    } else {
+      return null;
+    }
+  }
   // Overall pass
   m = str.match(
     /^(((\d{4})-(\d{2})(-(\d{2}))?|(\d{2})(\/(\d{2}))?\/(\d{4}))( +((\d{2}):(\d{2})(:(\d{2}))?(am|pm)?)( +(([+-]\d{2}):(\d{2})))?)?|(\d{2}):(\d{2})(:(\d{2}))?(am|pm)?)$/i
--- src/core/dataset/index.ts
+++ src/core/dataset/index.ts
@@ -5,3 +5,9 @@ export * from "./dataset";
 export { DatasetLoader } from "./loader";
 
 export { DatasetContext, TableContext, RowContext } from "./context";
+
+export {
+  convertColumnType,
+  inferColumnType,
+  inferAndConvertColumn
+} from "./data_types";
