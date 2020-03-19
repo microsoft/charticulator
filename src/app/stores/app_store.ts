efcfa9b1e995eabec07b2b917a078c68a3fe65cb
Ilfat Galiev <v-ilgal@microsoft.com>
2020-03-19 17:00:57 +0300

    Remove unused scales on save the chart

--- src/app/stores/app_store.ts
+++ src/app/stores/app_store.ts
@@ -158,6 +158,7 @@ export class AppStore extends BaseStore {
   }
 
   public saveState(): AppStoreState {
+    this.updateChartState();
     return {
       version: CHARTICULATOR_PACKAGE.version,
       dataset: this.dataset,
