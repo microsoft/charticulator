45975aa054fa851d48fe3d86e495623d066aff2d
Ilfat Galiev <v-ilgal@microsoft.com>
2020-06-01 18:04:28 +0300

    Fix displaying false in table view

--- src/app/views/dataset/table_view.tsx
+++ src/app/views/dataset/table_view.tsx
@@ -73,13 +73,13 @@ export class TableView extends React.Component<TableViewProps, {}> {
                   if (c.metadata.rawColumnName) {
                     return (
                       <td key={`${c.name}-${index}`}>
-                        {r[c.metadata.rawColumnName].toString()}
+                        {r[c.metadata.rawColumnName] != null && r[c.metadata.rawColumnName].toString()}
                       </td>
                     );
                   } else {
                     return (
                       <td key={`${c.name}-${index}`}>
-                        {r[c.name] && r[c.name].toString()}
+                        {r[c.name] != null && r[c.name].toString()}
                       </td>
                     );
                   }
