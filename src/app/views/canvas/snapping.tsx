0c95b47ad90d5b07f5ec5bf635e89f6ab731ac5c
Ilfat Galiev <v-ilgal@microsoft.com>
2020-06-03 18:51:30 +0300

    Merge master

--- src/app/views/canvas/snapping.tsx
+++ src/app/views/canvas/snapping.tsx
@@ -12,6 +12,8 @@ import {
 } from "../../../core";
 import { HandlesDragEvent } from "./handles";
 import { Actions } from "../../actions";
+import { GuideClass } from "../../../core/prototypes/guides";
+import { isType } from "../../../core/prototypes";
 
 export interface SnappableGuide<ElementType> {
   element: ElementType;
@@ -325,7 +327,24 @@ export class MarkSnappingSession extends SnappingSession<
     bound: Prototypes.Handles.Description,
     threshold: number
   ) {
-    super(guides.filter(x => x.element != element), bound, threshold);
+    super(
+      guides.filter(x => {
+        // element cannot snap to itself
+        if (x.element === element) {
+          return false;
+        }
+        // special rules for guides
+        if (element.classID === GuideClass.classID) {
+          // guide cannot snap to a mark
+          if (x.element && isType(x.element.classID, "mark")) {
+            return false;
+          }
+        }
+        return true;
+      }),
+      bound,
+      threshold
+    );
 
     this.mark = mark;
     this.element = element;
