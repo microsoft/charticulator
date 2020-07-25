5e1017ba57d43a04f6517a39363cc8a3483de9e8
Dan Marshall <danmar@microsoft.com>
2020-07-24 21:43:09 -0700

    move to snapping/chart.tsx

--- src/app/views/canvas/chart_editor.tsx
+++ src/app/views/canvas/chart_editor.tsx
@@ -37,11 +37,8 @@ import { DropZoneView } from "./dropzone";
 import { EditingLink } from "./editing_link";
 import { HandlesView } from "./handles";
 import { ResizeHandleView } from "./handles/resize";
-import {
-  ChartSnappableGuide,
-  ChartSnappingSession,
-  MoveSnappingSession
-} from "./snapping";
+import { ChartSnappableGuide, ChartSnappingSession } from "./snapping/chart";
+import { MoveSnappingSession } from "./snapping";
 
 export interface ChartEditorViewProps {
   store: AppStore;
@@ -57,11 +54,11 @@ export interface ChartEditorViewState {
   currentCreationOptions?: string;
   currentSelection: Selection;
   dropZoneData:
-    | {
-        data?: DragData.DropZoneData;
-        layout?: DragData.ScaffoldType;
-      }
-    | false;
+  | {
+    data?: DragData.DropZoneData;
+    layout?: DragData.ScaffoldType;
+  }
+  | false;
   isSolving: boolean;
 }
 
@@ -728,9 +725,9 @@ export class ChartEditorView
             ) : null}
             {Prototypes.isType(layout.classID, "plot-segment")
               ? this.renderMarkHandlesInPlotSegment(
-                  layout as Specification.PlotSegment,
-                  layoutState as Specification.PlotSegmentState
-                )
+                layout as Specification.PlotSegment,
+                layoutState as Specification.PlotSegmentState
+              )
               : null}
             <HandlesView
               handles={handles}
@@ -1075,7 +1072,7 @@ export class ChartEditorView
     const height = this.state.viewHeight;
     const transform = `translate(${this.state.zoom.centerX},${
       this.state.zoom.centerY
-    }) scale(${this.state.zoom.scale})`;
+      }) scale(${this.state.zoom.scale})`;
     return (
       <div className="chart-editor-view">
         <div className="chart-editor-canvas-view" ref="canvasContainer">
