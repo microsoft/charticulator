bc36b524c476a9c65e91dad50c8d57251d17cfa9
Ilfat Galiev <v-ilga@microsoft.com>
2020-10-13 19:00:56 +0300

    Align plot segment attribute buttons

--- src/core/prototypes/plot_segments/region_2d/base.ts
+++ src/core/prototypes/plot_segments/region_2d/base.ts
@@ -2212,14 +2212,18 @@ export class Region2DConstraintBuilder {
       m.sectionHeader("Sub-layout"),
       m.row(
         "Type",
-        m.inputSelect(
-          { property: "sublayout", field: "type" },
-          {
-            type: "radio",
-            options: options.map(x => x.value),
-            icons: options.map(x => x.icon),
-            labels: options.map(x => x.label)
-          }
+        m.horizontal(
+          [0, 0],
+          null,
+            m.inputSelect(
+              { property: "sublayout", field: "type" },
+              {
+                type: "radio",
+                options: options.map(x => x.value),
+                icons: options.map(x => x.icon),
+                labels: options.map(x => x.label)
+              }
+            ),
         )
       ),
       ...extra
