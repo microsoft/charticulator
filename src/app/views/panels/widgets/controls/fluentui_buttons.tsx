c61806cabb30d97d40e7b6aea4ae9b1eb13b279f
Ilfat Galiev <zbritva@gmail.com>
2021-01-26 19:59:08 +0300

    Add styled-components

---
+++ src/app/views/panels/widgets/controls/fluentui_buttons.tsx
@@ -0,0 +1,19 @@
+
+import styled from "styled-components";
+
+export const FluentButton = styled.div`
+display: inline-block;
+padding: 0px 4px 0px 4px;
+button {
+  min-width: unset;
+  padding: 4px;
+}
+`;
+
+export const FluentActionButton = styled.div`
+button {    
+    border: 1px solid;
+    height: 32px;
+    width: 100%;
+}
+`
