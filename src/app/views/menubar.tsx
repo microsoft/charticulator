d8c976e3b2422571012161eab8586ff3097f04f7
Dan Marshall <danmar@microsoft.com>
2021-02-01 19:31:28 -0800

    autoformat

--- src/app/views/menubar.tsx
+++ src/app/views/menubar.tsx
@@ -403,60 +403,58 @@ export class MenuBar extends ContextedComponent<MenuBarProps, {}> {
   public renderDelete(showLabel: boolean) {
     return (
       <MenuButton
-          url={R.getSVGIcon("toolbar/trash")}
-          title={strings.menuBar.reset}
-          text={strings.menuBar.reset}
-          onClick={() => {
-            if (isInIFrame()) {
-              globals.popupController.showModal(
-                (context) => {
-                  return (
-                    <div
-                      onMouseDown={(e) => {
-                        e.stopPropagation();
-                      }}
-                      className={"charticulator__reset_chart_dialog"}
-                    >
-                      <div
-                        className={"charticulator__reset_chart_dialog-inner"}
-                      >
-                        <>
-                          <p>{strings.dialog.resetConfirm}</p>
-                          <div
-                            className={
-                              "charticulator__reset_chart_dialog-buttons"
-                            }
-                          >
-                            <Button
-                              text={strings.button.yes}
-                              onClick={() => {
-                                this.context.store.dispatcher.dispatch(
-                                  new Actions.Reset()
-                                );
-                                context.close();
-                              }}
-                            />
-                            <Button
-                              text={strings.button.no}
-                              onClick={() => {
-                                context.close();
-                              }}
-                            />
-                          </div>
-                        </>
-                      </div>
+        url={R.getSVGIcon("toolbar/trash")}
+        title={strings.menuBar.reset}
+        text={strings.menuBar.reset}
+        onClick={() => {
+          if (isInIFrame()) {
+            globals.popupController.showModal(
+              (context) => {
+                return (
+                  <div
+                    onMouseDown={(e) => {
+                      e.stopPropagation();
+                    }}
+                    className={"charticulator__reset_chart_dialog"}
+                  >
+                    <div className={"charticulator__reset_chart_dialog-inner"}>
+                      <>
+                        <p>{strings.dialog.resetConfirm}</p>
+                        <div
+                          className={
+                            "charticulator__reset_chart_dialog-buttons"
+                          }
+                        >
+                          <Button
+                            text={strings.button.yes}
+                            onClick={() => {
+                              this.context.store.dispatcher.dispatch(
+                                new Actions.Reset()
+                              );
+                              context.close();
+                            }}
+                          />
+                          <Button
+                            text={strings.button.no}
+                            onClick={() => {
+                              context.close();
+                            }}
+                          />
+                        </div>
+                      </>
                     </div>
-                  );
-                },
-                { anchor: null }
-              );
-            } else {
-              if (confirm(strings.dialog.resetConfirm)) {
-                new Actions.Reset().dispatch(this.context.store.dispatcher);
-              }
+                  </div>
+                );
+              },
+              { anchor: null }
+            );
+          } else {
+            if (confirm(strings.dialog.resetConfirm)) {
+              new Actions.Reset().dispatch(this.context.store.dispatcher);
             }
-          }}
-        />
+          }
+        }}
+      />
     );
   }
 
@@ -512,11 +510,10 @@ export class MenuBar extends ContextedComponent<MenuBarProps, {}> {
         {this.context.store.editorType === "embedded"
           ? this.renderSaveEmbedded()
           : null}
-        {this.context.store.editorType === "embedded" ?
-        (
+        {this.context.store.editorType === "embedded" ? (
           <>
-          <span className="charticulator__menu-bar-separator" />
-          {this.renderExportImportButtons(props)}
+            <span className="charticulator__menu-bar-separator" />
+            {this.renderExportImportButtons(props)}
           </>
         ) : null}
         <span className="charticulator__menu-bar-separator" />
