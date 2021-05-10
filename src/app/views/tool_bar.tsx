d06e9b72febf7c9be6a5f4a9a36e6f7e91237a4b
Ilfat Galiev <v-igaliev@microsoft.com>
2021-05-10 21:16:28 +0300

    Set defaults for data axis

--- src/app/views/tool_bar.tsx
+++ src/app/views/tool_bar.tsx
@@ -204,7 +204,7 @@ export class Toolbar extends ContextedComponent<
         classID="mark.data-axis"
         title={strings.toolbar.dataAxis}
         icon="mark/data-axis"
-        options={"{axis:{autoDomainMin: true,autoDomainMax: true}}"}
+        options={'{"axis":{"autoDomainMin": true,"autoDomainMax": true}}'}
       />
     );
   }
