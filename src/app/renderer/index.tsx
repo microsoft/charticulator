f873f46e903662a8820635e04f046aebc00bc6e9
Ramil Minyukov (Akvelon INC) <v-rminyukov@microsoft.com>
2021-08-31 14:24:56 +0300

    Fix NaN and null values

--- src/app/renderer/index.tsx
+++ src/app/renderer/index.tsx
@@ -319,6 +319,11 @@ export function renderGraphicalElementSVG(
     }
   }
 
+  const replaceNan = (value: number) => {
+    return isNaN(value) ? 0 : value;
+  };
+
+
   switch (element.type) {
     case "rect": {
       const rect = element as Graphics.Rect;
@@ -329,9 +334,9 @@ export function renderGraphicalElementSVG(
           className={options.className || null}
           style={style}
           x={Math.min(rect.x1, rect.x2)}
-          y={-Math.max(rect.y1, rect.y2)}
-          width={Math.abs(rect.x1 - rect.x2)}
-          height={Math.abs(rect.y1 - rect.y2)}
+          y={replaceNan(-Math.max(rect.y1, rect.y2))}
+          width={replaceNan(Math.abs(rect.x1 - rect.x2))}
+          height={replaceNan(Math.abs(rect.y1 - rect.y2))}
         />
       );
     }
