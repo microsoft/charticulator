0b4d80fce0fa2e501045982695126c1698b4b290
Ramil Minyukov (Akvelon INC) <v-rminyukov@microsoft.com>
2021-10-06 11:47:43 +0300

    Added zooming

--- src/app/views/canvas/mark_editor.tsx
+++ src/app/views/canvas/mark_editor.tsx
@@ -232,6 +232,13 @@ export class MarkEditorView extends ContextedComponent<
                 this.refSingleMarkView.doZoomAuto();
               }}
             />
+            <Button
+              icon="RectangleShape"
+              title={"Rectangle zoom"}
+              onClick={() => {
+                this.dispatch(new Actions.SetCurrentTool("rectangle-zoom"));
+              }}
+            />
           </div>
         </div>
       </div>
@@ -305,6 +312,24 @@ export class SingleMarkView
         scale: newScale,
       },
     });
+  }
+
+  public doCustomZoom(cx: number, cy: number, width: number, height: number) {
+    const newCX = cx + this.props.width / 2;
+    const newCY = cy + this.props.height / 2;
+    const newScale =
+      this.props.width > this.props.height
+        ? this.props.height / height
+        : this.props.width / width;
+
+    this.setState({
+      zoom: {
+        centerX: newCX,
+        centerY: newCY,
+        scale: 1,
+      },
+    });
+    this.doZoom(newScale);
   }
 
   public doZoomAuto() {
@@ -1552,6 +1577,18 @@ export class SingleMarkView
         //     };
         //   }
         //   break;
+        case "rectangle-zoom":
+          {
+            mode = "rectangle";
+            onCreate = (x1, y1, x2, y2) => {
+              const width = Math.abs(x2[0] - x1[0]);
+              const height = Math.abs(y2[0] - y1[0]);
+              const centerX = Math.min(x2[0], x1[0]) + width / 2;
+              const centerY = Math.min(y2[0], y1[0]) + height / 2;
+              this.doCustomZoom(centerX, centerY, width, height);
+            };
+          }
+          break;
       }
       return (
         <CreatingComponent
