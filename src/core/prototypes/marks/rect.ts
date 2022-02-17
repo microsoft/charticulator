e399a3562587fc667ee8a38a497eec38b838c693
MrRamka <v-rminyukov@microsoft.com>
2022-02-17 17:43:15 +0300

    Update x and y position

--- src/core/prototypes/marks/rect.ts
+++ src/core/prototypes/marks/rect.ts
@@ -837,25 +837,23 @@ export class RectElementClass extends EmphasizableMarkClass<
       properties.orientation == OrientationType.HORIZONTAL &&
       helper.coordinateSystem instanceof CartesianCoordinates
     ) {
+      const xPosition = x2 > x1 ? x1 + minHalfWidth : x1 - minHalfWidth;
       if (properties.cometMark == true) {
-        pathMaker.moveTo(
-          x2 > x1 ? x1 + minHalfWidth : x1 - minHalfWidth,
-          Math.max(y1, y2)
-        );
+        pathMaker.moveTo(xPosition, Math.max(y1, y2));
         helper.arcTo(
           pathMaker,
           minHalfWidth,
           halfYWidth,
-          x2 > x1 ? x1 + minHalfWidth : x1 - minHalfWidth,
+          xPosition,
           Math.max(y1, y2),
-          x2 > x1 ? x1 + minHalfWidth : x1 - minHalfWidth,
+          xPosition,
           Math.min(y1, y2),
           x2 > x1 ? 0 : 1
         );
 
         helper.lineTo(
           pathMaker,
-          x2 > x1 ? x1 + minHalfWidth : x1 - minHalfWidth,
+          xPosition,
           Math.max(y1, y2),
           x2,
           (y1 + y2) / 2,
@@ -870,25 +868,23 @@ export class RectElementClass extends EmphasizableMarkClass<
         properties.cometMark == true &&
         helper.coordinateSystem instanceof CartesianCoordinates
       ) {
-        pathMaker.moveTo(
-          Math.max(x1, x2),
-          y2 > y1 ? y1 + minHalfWidth : y1 - minHalfWidth
-        );
+        const yPosition = y2 > y1 ? y1 + minHalfWidth : y1 - minHalfWidth;
+        pathMaker.moveTo(Math.max(x1, x2), yPosition);
         helper.arcTo(
           pathMaker,
           halfXWidth,
           minHalfWidth,
           Math.max(x1, x2),
-          y2 > y1 ? y1 + minHalfWidth : y1 - minHalfWidth,
+          yPosition,
           Math.min(x1, x2),
-          y2 > y1 ? y1 + minHalfWidth : y1 - minHalfWidth,
+          yPosition,
           y2 > y1 ? 1 : 0
         );
 
         helper.lineTo(
           pathMaker,
           Math.max(x1, x2),
-          y2 > y1 ? y1 + minHalfWidth : y1 - minHalfWidth,
+          yPosition,
           (x1 + x2) / 2,
           y2,
           false
