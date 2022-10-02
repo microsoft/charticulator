f9b86ed1e8b7e1b1b65430821a652b84fccb7d73
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-02 13:53:17 -0400

    spelling: object
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/container/chart_template.ts
+++ src/container/chart_template.ts
@@ -124,7 +124,7 @@ export class ChartTemplate {
   }
 
   /**
-   * Creates instance of chart object from template. Chart objecty can be loaded into container to display it in canvas
+   * Creates instance of chart object from template. Chart object can be loaded into container to display it in canvas
    * On editing this method ensure that you made correspond changes in template builder ({@link ChartTemplateBuilder}).
    * Any exposed into template objects should be initialized here
    */
