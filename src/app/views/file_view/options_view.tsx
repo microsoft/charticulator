bd7bf51aeea0b0feb828bb5b131a8631d7c66070
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-02 13:53:20 -0400

    spelling: removal
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/app/views/file_view/options_view.tsx
+++ src/app/views/file_view/options_view.tsx
@@ -80,7 +80,7 @@ export const FileViewOptionsView: React.FC<FileViewOptionsProps> = () => {
             <select
               onChange={(e) => {
                 const isDecimalDot =
-                  e.target.options[e.target.selectedIndex].value === ","; // values is removeal
+                  e.target.options[e.target.selectedIndex].value === ","; // values is removal
                 changeLocaleFileFormat({
                   ...localeFileFormat,
                   numberFormat: {
